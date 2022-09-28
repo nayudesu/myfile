0a71ab28d7b7c5583a3d68275b89428324f61153
久保菜友加 <srkm.149@gmail.com>
2022-09-28 14:04:57 +0900

    newインスタンス

--- practice.java
+++ practice.java
@@ -1,5 +1,13 @@
 class Hello {
      public static void main(String[] args){
-         System.out.println("Hello, world!");
+      String s1 = "123";
+      // new ＝インスタンス化（実体を作る）
+      String s2 = new Srting("123");
+
+      // 参照が正しいかを比較
+      if (s1 == s2) {
+        System.out.println("s1とs2は等しい")
+      }
+      // 
      }
 }
