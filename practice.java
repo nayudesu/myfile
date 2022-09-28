2a23c588bf18207c3616d35d06b241dd04ad5368
久保菜友加 <srkm.149@gmail.com>
2022-09-28 14:09:15 +0900

    equalsメソッド

--- practice.java
+++ practice.java
@@ -2,12 +2,17 @@ class Hello {
      public static void main(String[] args){
       String s1 = "123";
       // new ＝インスタンス化（実体を作る）
-      String s2 = new Srting("123");
+      String s2 = new String("123");
 
       // 参照が正しいかを比較
       if (s1 == s2) {
-        System.out.println("s1とs2は等しい")
+        System.out.println("s1とs2は等しい");
       }
-      // 
+
+      // 値が正しいか比較
+      // equals =値が等しいtrue,等しくない時にはfalse
+      if(s1.equals(s2)) {
+        System.out.println("s1とs2の値は等しい");
+      }
      }
 }
